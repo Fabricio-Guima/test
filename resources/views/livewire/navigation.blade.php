51fbc8ab6d3611ffc44e76c1d5d3002d0b98ed4d
Fabricio-Guima <fsgkof@gmail.com>
2024-02-27 00:16:31 -0300

    using dropdowns components on  main navigation

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -20,5 +20,79 @@
         {{-- livewire --}}
         @livewire('search')
 
+        <div class="mx-6 relative">
+            @auth
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button
+                            class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                                alt="{{ Auth::user()->name }}" />
+                        </button>
+
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Manage Account') }}
+                        </div>
+
+                        <x-dropdown-link href="{{ route('profile.show') }}">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        {{-- @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                        <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                            {{ __('API Tokens') }}
+                        </x-dropdown-link>
+                        @endif --}}
+
+                        <div class="border-t border-gray-200 dark:border-gray-600"></div>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
+
+                            <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            @else
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+                    </x-slot>
+
+                    <x-slot name="content">
+
+                        <x-dropdown-link href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </x-dropdown-link>
+
+                        <x-dropdown-link href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </x-dropdown-link>
+
+                        {{-- @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                        <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                            {{ __('API Tokens') }}
+                        </x-dropdown-link>
+                        @endif --}}
+
+                        {{-- <div class="border-t border-gray-200 dark:border-gray-600"></div> --}}
+
+
+                    </x-slot>
+                </x-dropdown>
+
+            @endauth
+
+        </div>
+
+        @livewire('dropdown-cart')
+
     </div>
 </header>
