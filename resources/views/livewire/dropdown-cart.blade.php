29f0afe4ea3cb0afea37d1fe26608308a6c49038
Fabricio-Guima <fsgkof@gmail.com>
2024-02-27 00:16:04 -0300

    dropdown-cart blade component

---
+++ resources/views/livewire/dropdown-cart.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <x-dropdown width="96">
+        <x-slot name="trigger">
+            <x-cart />
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="py-6 px-4">
+                <p class="text-center text-gray-700">Não há itens no carrinho</p>
+            </div>
+        </x-slot>
+    </x-dropdown>
+</div>
