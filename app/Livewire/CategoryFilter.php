cdda7dae9a71adbcf34ca3994cb62628140df46b
Fabricio-Guima <fsgkof@gmail.com>
2024-03-05 17:44:39 -0300

    grid products

---
+++ app/Livewire/CategoryFilter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use App\Models\Subcategory;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Component;
+
+use Livewire\WithPagination;
+
+class CategoryFilter extends Component
+{
+    // com isso, a página não é recarregada, somente os produtos
+    use WithPagination;
+    public $category;
+    public $subcategoria;
+    public $marca;
+
+    public $view = 'grid';
+
+    public function resetFilter()
+    {
+        $this->reset(['subcategoria', 'marca']);
+    }
+    public function render()
+    {
+        // $products = $this->category->products()
+        //     ->where('status', 2)
+        //     ->paginate(20);
+
+       $products = Product::query()
+        ->whereHas('subcategory.category', function (Builder $query) {
+            $query->where('id', $this->category->id);
+        })
+        ->when($this->subcategoria, function ($query) {
+            return $query->whereHas('subcategory', function (Builder $query) {
+                $query->where('name', $this->subcategoria);
+            });
+        })
+        ->when($this->marca, function ($query) {
+            return $query->whereHas('brand', function (Builder $query) {
+                $query->where('name', $this->marca);
+            });
+        })
+        ->paginate(20);
+
+
+        return view('livewire.category-filter', compact('products'));
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
         @endif
 
         <!-- Page Content -->
-        <main class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <main class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             {{ $slot }}
         </main>
     </div>
